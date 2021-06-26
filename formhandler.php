927b8b01b7ef084023261068ece2869cc7aa9b48
brightwhitesnow <bennguyen0808@gmail.com>
2021-06-26 10:10:37 -0400

    asdfghjkl

--- formhandler.php
+++ formhandler.php
@@ -3,12 +3,12 @@
     $email = $_POST['email'];
     $message = $_POST['message'];
 
-    $email_from = $email;
+    $email_from = 'bennguyen0808@gmail.com';
     $email_subject = 'BB Library Form Submission';
     $email_body = "Name: $name./n".
                     "Email: $email./n".
                 "Message: $message./n";
 
-    $to = "bennguyen0808@gmail.com";
+    $to = "ductuan.bbl@gmail.com";
     mail($to,$email_subject,$email_body);
 ?>
